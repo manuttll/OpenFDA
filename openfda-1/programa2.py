79aed2d6a22438e7ec542ff0b41af01158b04106
manuttll <manutristanlo@hotmail.com>
2018-04-06 11:16:04 +0200

    práctica

--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -1,3 +1,5 @@
+#Este programa es igual que el anterior exceptuando que como recurso añadimos limit=10. El resto del proceso de conexión es igual.
+
 import http.client
 import json
 
@@ -8,7 +10,7 @@ conn.request("GET", "/drug/label.json?limit=10", None, cabecera)
 r1 = conn.getresponse()
 resp_des = r1.read().decode("utf-8")
 conn.close()
-
+#Una vez obtenida la información se reorganiza como anteriormente y con un pequeño bucle imprimimos los ID.
 resp = json.loads(resp_des)
 for i in range (len (resp['results'])):
     medicamento_info=resp['results'][i]
--- openfda-2/programa3.py
+++ openfda-2/programa3.py
@@ -1,3 +1,6 @@
+#Para este programa volvemos a importar los módulos y usamos la función skip. Creamos un bucle infinito y establecemos la conexión
+#igual que antes, añadiendo el skip, el limit=100 y la búsqueda específica en el recurso.
+
 import http.client
 import json
 
@@ -11,12 +14,12 @@ while True:
     resp_des = r1.read().decode("utf-8")
     conn.close()
 
+#Mediante un bucle for imprimimos la información que se desea, en caso de que la respuesta contenga 100 medicamentos (el máximo posible)
+# se mantiene el bucle While, y hasta que no se devuelven menos de 100 no se rompe.
     resp = json.loads(resp_des)
     for i in range (len (resp['results'])):
         medicamento_info=resp['results'][i]
-        print ('ID: ',medicamento_info['id'])
-        if (medicamento_info['openfda']):
-            print('Fabricante: ', medicamento_info['openfda']['manufacturer_name'][0])
+        print('Fabricante: ', medicamento_info['openfda']['manufacturer_name'][0])
 
     if(len(resp['results'])<100):
         break
